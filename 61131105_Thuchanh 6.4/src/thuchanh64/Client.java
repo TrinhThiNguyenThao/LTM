05d9d7921c399e7df7671e813409de3efba73046
TrinhThiNguyenThao <102137364+TrinhThiNguyenThao@users.noreply.github.com>
2022-04-06 19:12:47 +0700

    Add files via upload

--- 61131105_Thuchanh 6.4/src/thuchanh64/Client.java
+++ 61131105_Thuchanh 6.4/src/thuchanh64/Client.java
@@ -1,5 +1,20 @@
 package thuchanh64;
 
+import java.io.BufferedReader;
+
+import java.io.BufferedWriter;
+
+import java.io.InputStream;
+
+import java.io.InputStreamReader;
+
+import java.io.OutputStream;
+
+import java.io.OutputStreamWriter;
+
+import java.net.Socket;
+
+import java.util.Scanner;
 
 public class Client {
 
